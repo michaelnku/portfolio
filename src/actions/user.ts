5a3a86296c40a3ab90e7d4bddc3996b4de979d8b
michael <nkumichael1@gmail.com>
2026-01-11 00:48:55 +0100

    adding initials as avatar fallback

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -19,6 +19,7 @@ import { AuthError } from "next-auth";
 import { ADMIN_LOGIN_REDIRECT } from "@/routes";
 import { CurrentUser } from "@/lib/currentUser";
 import { revalidatePath } from "next/cache";
+import { Prisma } from "@/generated/prisma/client";
 
 // create user action
 export const createUser = async (values: UserSchemaType) => {
@@ -124,7 +125,7 @@ export async function updateUserProfile(values: updateUserSchemaType) {
     data: {
       name,
       username,
-      profileImage,
+      profileImage: profileImage === null ? Prisma.JsonNull : profileImage,
     },
   });
 

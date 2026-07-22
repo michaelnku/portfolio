ffd89236ab8dfad4e33513aeb9893ed0dab73b86
Michaelnku <nkumichael1@gmail.com>
2026-07-22 09:09:05 +0100

    refactored prisma file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,35 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-if (!process.env.ACCELERATE_URL) {
-  throw new Error("ACCELERATE_URL is not set");
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    accelerateUrl: process.env.ACCELERATE_URL,
-  }).$extends(withAccelerate());
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from "@/generated/prisma/client";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// if (!process.env.ACCELERATE_URL) {
+//   throw new Error("ACCELERATE_URL is not set");
+// }
+
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     accelerateUrl: process.env.ACCELERATE_URL,
+//   }).$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
